42389d073137105af37333ab5acf09c49f32c420
LeonardoTonon1234 <146954166+LeonardoTonon1234@users.noreply.github.com>
2025-11-07 01:29:44 -0300

    Create database and filmes table in script.sql

--- backend/database/script.sql
+++ backend/database/script.sql
@@ -1,1 +1,12 @@
 
+CREATE DATABASE catalogofilmes;
+USE catalogofilmes;
+
+CREATE TABLE filmes (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(100) NOT NULL,
+  diretor VARCHAR(100) NOT NULL,
+  genero VARCHAR(50) NOT NULL,
+  ano_lancamento INT NOT NULL,
+  avaliacao DECIMAL(3,1) NOT NULL
+);
