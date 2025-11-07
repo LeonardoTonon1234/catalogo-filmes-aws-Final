fc888a1ac1435f1c03aeb27c43f2d5cc9858ce50
LeonardoTonon1234 <146954166+LeonardoTonon1234@users.noreply.github.com>
2025-11-07 01:29:04 -0300

    Implement Lambda function to retrieve movie data
    Added a Lambda function to fetch movie data from a MySQL database.

--- backend/lambda/lambda_function.py
+++ backend/lambda/lambda_function.py
@@ -1,1 +1,22 @@
+import json
+import pymysql
+
+def lambda_handler(event, context):
+    connection = pymysql.connect(
+        host='catalogofilmes-db.c762082m6il2.us-east-1.rds.amazonaws.com',
+        user='admin',
+        password='SUA_SENHA_AQUI',
+        database='catalogofilmes'
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, titulo, diretor, genero, ano_lancamento, avaliacao FROM filmes")
+        filmes = cursor.fetchall()
+
+    connection.close()
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(filmes)
+    }
 
